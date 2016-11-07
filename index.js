9d4cd30f85a939470308d10bb86382d38acf2b42
Casey Webb <notcaseywebb@gmail.com>
2016-11-07 14:32:09 -0500

    WIP

--- index.js
+++ index.js
@@ -5,62 +5,64 @@ const path = require('path')
 const Koa = require('koa')
 const _ = require('lodash')
 const mkdirp = require('mkdirp')
+const fs = require('fs-promise')
 let logger; const { error, info, verbose } = logger = require('winston')
-
-const app = new Koa()
-let config = {}
-
-try {
-  config = require('./config')
-} catch (e) {
-  verbose('No config.js found')
-  config = {}
-}
-
-_(config)
-  .defaults({
-    environment: 'development',
-    password: '',
-    port: 8080,
-    host: '0.0.0.0',
-    use_https: false,
-    secret: 'not a good secret',
-    log_level: 'info'
-  })
-  .extendWith({
-    environment: process.env.NODE_ENV,
-    password: process.env.HUBBARD_PASSWORD,
-    port: process.env.HUBBARD_PORT,
-    host: process.env.HUBBARD_HOST,
-    use_https: process.env.HUBBARD_USE_HTTPS,
-    secret: process.env.HUBBARD_SECRET,
-    github_access_token: process.env.HUBBARD_GITHUB_ACCESS_TOKEN,
-    log_level: process.env.HUBBARD_LOG_LEVEL
-  }, (c, env) => env || c)
-  .value()
-
-app.keys = [config.secret]
-
-console.log(process.env)
-
-if (!config.github_access_token) {
-  error(`
-    No GitHub Access Token Supplied:
-      Please generate a token at https://github.com/settings/tokens with
-      repo and admin:repo_hook permissions, and supply that token in config.js
-      as "github_access_token"
-  `)
-
-  throw new Error('Missing GitHub Access Token')
-}
-
-logger.level = config.log_level
-logger.remove(logger.transports.Console)
-logger.add(logger.transports.Console, { colorize: true })
 
 start()
 
 async function start() {
+  const app = new Koa()
+  let config = {}
+
+  try {
+    config = require('./config')
+  } catch (e) {
+    verbose('No config.js found, creating...')
+    await fs.writeFile(path.join(__dirname, 'config.js'), 'use strict\n\nmodule.exports = {}')
+    config = require('./config')
+  }
+
+  _(config)
+    .defaults({
+      environment: 'development',
+      password: '',
+      port: 8080,
+      host: '0.0.0.0',
+      use_https: false,
+      secret: 'not a good secret',
+      log_level: 'info'
+    })
+    .extendWith({
+      environment: process.env.NODE_ENV,
+      password: process.env.HUBBARD_PASSWORD,
+      port: process.env.HUBBARD_PORT,
+      host: process.env.HUBBARD_HOST,
+      use_https: process.env.HUBBARD_USE_HTTPS,
+      secret: process.env.HUBBARD_SECRET,
+      github_access_token: process.env.HUBBARD_GITHUB_ACCESS_TOKEN,
+      log_level: process.env.HUBBARD_LOG_LEVEL
+    }, (c, env) => env || c)
+    .value()
+
+  app.keys = [config.secret]
+
+  console.log(process.env)
+
+  if (!config.github_access_token) {
+    error(`
+      No GitHub Access Token Supplied:
+        Please generate a token at https://github.com/settings/tokens with
+        repo and admin:repo_hook permissions, and supply that token in config.js
+        as "github_access_token"
+    `)
+
+    throw new Error('Missing GitHub Access Token')
+  }
+
+  logger.level = config.log_level
+  logger.remove(logger.transports.Console)
+  logger.add(logger.transports.Console, { colorize: true })
+
   const host = config.host
   const port = config.port
 
